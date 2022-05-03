e152f48d4a5a1cc4fd0e52fd8091ce9fcf4baea5
AlyssaAlarid <alarid@colostate.edu>
2022-05-03 20:59:46 +0000

    MyFourthProject

---
+++ MyFourthProject/App copy.js
@@ -0,0 +1,15 @@
+import React, {Component} from 'react';
+import {Text, View } from 'react-native';
+
+export default class MyApp extends Component {
+  render() {
+  return (
+    <View style={{padding: 60}}>
+      <Text>Open up App.js to start working on your app!</Text>
+      <Text>I like using the class Component!</Text>
+    </View>
+  );
+}
+}
+
+
