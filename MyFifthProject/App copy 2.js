ec69adc857f44c8af3c3892cc5e4efe24904a5a2
AlyssaAlarid <alarid@colostate.edu>
2022-05-05 00:43:41 +0000

    Style and layout Project

---
+++ MyFifthProject/App copy 2.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import { View } from 'react-native';
+
+export default function App() {
+  return (
+    <View> 
+      <View style={{width: 50, height: 50, backgroundColor: 'blue'}}/>
+      <View style={{width: 100, height: 100, backgroundColor: 'yellow'}}/>
+      <View style={{width: 150, height: 150, backgroundColor: 'orange'}}/>
+      <View style={{width: 200, height: 200, backgroundColor: 'pink'}}/>
+      <View style={{width: 250, height: 250, backgroundColor: 'purple'}}/>
+
+
+    </View>
+  );
+}
+
+
--- MyFifthProject/App copy 3.js
+++ MyFifthProject/App copy 3.js
@@ -3,8 +3,8 @@ import { View } from 'react-native';
 
 export default function App() {
   return (
-    <View style={{flex: 1, flexDirection: 'column', justifyContent: 'center', alignItems: 'baseline'}}> 
-      <View style={{height:50, backgroundColor: 'pink', alignSelf: 'stretch'}}/>
+    <View style={{flex: 1, flexDirection: 'row', marginTop: 100}}> 
+      <View style={{width:50, height:50, backgroundColor: 'pink'}}/>
       <View style={{width:50, height:50, backgroundColor:'yellow' }}/>
       <View style={{width:50, height:50, backgroundColor: 'blue'}}/>
     </View>
